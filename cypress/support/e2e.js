5f66d9f75be460345c5adc8d8708556900df9613
Chris Archer <chrisarchercanada@gmail.com>
2025-05-30 10:00:22 -0700

    Removed UI tests and Cypress directory

--- cypress/support/e2e.js
+++
@@ -1,4 +0,0 @@
-// This file is automatically loaded before your test files.
-// You can use it to set up global configurations and behaviors.
-
-import './commands'; // Import custom commands if you have any
